d93ad0d6088d05eae1c8c034917b0d26f38ca578
David Marsh <marshmonkey@gmail.com>
2026-02-27 13:29:56 -0800

    plan-312 phase-1: Implement src/discord/ (2 files)

--- src/discord/tool-aware-queue.ts
+++ src/discord/tool-aware-queue.ts
@@ -24,7 +24,7 @@ export class ToolAwareQueue {
 
   constructor(emit: (action: DisplayAction) => void, opts?: ToolAwareQueueOpts) {
     this.emit = emit;
-    this.flushDelayMs = opts?.flushDelayMs ?? 2000;
+    this.flushDelayMs = opts?.flushDelayMs ?? 800;
     this.postToolDelayMs = opts?.postToolDelayMs ?? 500;
   }
 
